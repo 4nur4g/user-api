62574776404f852e3253d339dc2d2659e15ca122
Anurag Pradhan <anuragp936@gmail.com>
2023-05-28 20:09:51 +0530

    Implement security

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// Security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const express = require('express');
 const app = express();
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy',1)
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 150
+}))
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 const ConnectDB = require('./db/connect');
 const usesrRoutes = require('./routes/users');
