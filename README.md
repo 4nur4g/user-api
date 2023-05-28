c7a1d5b920ffc0a61809eb2ddcce1d975dd06621
Anurag Pradhan <anuragp936@gmail.com>
2023-05-28 20:37:57 +0530

    Add readme, further refinement

---
+++ README.md
@@ -0,0 +1,44 @@
+## Users API using Node/Express
+
+This is a Node application that uses Express and Mongoose to create a RESTful API for managing users. The API allows you to perform CRUD (Create, Read, Update, Delete) operations on user documents stored in MongoDB.
+
+### Architecture
+
+The application follows the Model-Router-Controller (MRC) architecture for separating the concerns of data, routing, and logic. The main components are:
+
+- **Model**: Defines the schema and methods for user documents using Mongoose.
+- **Router**: Handles the HTTP requests and maps them to the appropriate controller functions using Express.
+- **Controller**: Validates the request parameters, query, and body, performs the business logic, and sends the response with the correct status code and data.
+
+The root file of the application is `app.js`. The base URL for the API is `/api/v1/users`.
+
+### Endpoints
+
+The API supports the following endpoints:
+
+- `GET /`: Returns a list of all users with optional sorting and pagination parameters.
+- `POST /`: Creates a new user with the given data in the request body and returns the created user.
+- `GET /:id`: Returns a single user with the specified ID in the request parameter.
+- `PATCH /:id`: Updates one or more fields of the user with the specified ID in the request parameter and returns the updated user.
+- `DELETE /:id`: Deletes the user with the specified ID in the request parameter and returns a success message.
+
+### Sorting and Pagination
+
+The API supports sorting and pagination for the `GET /` endpoint. You can use the following query parameters to customize the results:
+
+- `sort`: Specifies the property on which to sort the results. To sort in ascending order, use the property name as the value. To sort in descending order, use a minus sign (-) before the property name. For example, `sort=first_name` will sort by name in ascending order, while `sort=-first_name` will sort by name in descending order. By default, the API returns the sorted data based on the `first_name` property.
+- `page`: Specifies the page number of the results to return. The default value is 1.
+- `limit`: Specifies the number of results per page. The default value is 10.
+
+### Error Handling
+
+The application uses a custom error handling middleware to handle errors. The middleware catches all errors thrown by the controllers or other middlewares and sends a response with an appropriate status code and message. The application also uses the `express-async-errors` library to automatically direct all errors to this middleware.
+
+### Security
+
+The application uses several packages to enhance security, such as:
+
+- **helmet**: Sets various HTTP headers to protect against common attacks.
+- **cors**: Enables cross-origin resource sharing (CORS) to allow requests from different origins.
+- **xss**: Sanitizes user input to prevent cross-site scripting (XSS) attacks.
+- **rateLimiter**: Limits the number of requests per IP address to prevent denial-of-service (DoS) attacks.
--- controllers/users.js
+++ controllers/users.js
@@ -3,81 +3,78 @@ const { StatusCodes } = require('http-status-codes');
 const { NotFoundError } = require('../errors');
 
 const getAllUsers = async (req, res) => {
-    const { sort } = req.query;
+  const { sort } = req.query;
 
-    let result = User.find({})
+  let result = User.find({});
 
-    if (sort) {
-        console.log(sort)
-        result = result.sort(sort);
-    } else {
-        result = result.sort('first_name');
-    }
+  if (sort) {
+    console.log(sort);
+    result = result.sort(sort);
+  } else {
+    result = result.sort('first_name');
+  }
 
-    const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 10;
-    const skip = (page - 1) * limit;
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
 
-    result = await result.skip(skip).limit(limit);
-    const totalUsers = await User.countDocuments({})
+  result = await result.skip(skip).limit(limit);
+  const totalUsers = await User.countDocuments({});
 
-    res.status(StatusCodes.OK).json({totalUsers, page,limit, users: result});
+  res.status(StatusCodes.OK).json({ totalUsers, page, limit, users: result });
 };
 const getUser = async (req, res) => {
-    const {
-       params:{ id:  userId },
-    } = req;
+  const {
+    params: { id: userId },
+  } = req;
 
-    const user = await User.findOne({
-        _id: userId,
-    });
-    if (!user) {
-        throw new NotFoundError(`No user with id ${userId}`);
-    }
-    res.status(StatusCodes.OK).json({ user });
+  const user = await User.findOne({
+    _id: userId,
+  });
+  if (!user) {
+    throw new NotFoundError(`No user with id ${userId}`);
+  }
+  res.status(StatusCodes.OK).json({ user });
 };
 
 const deleteUser = async (req, res) => {
-    const {
-        params:{ id:  userId },
-    } = req;
+  const {
+    params: { id: userId },
+  } = req;
 
-
-    const user = await User.findOneAndRemove({
-        _id: userId,
-    });
-    if (!user) {
-        throw new NotFoundError(`No user with id ${userId}`);
-    }
-    res.status(StatusCodes.OK).json({ user });
+  const user = await User.findOneAndRemove({
+    _id: userId,
+  });
+  if (!user) {
+    throw new NotFoundError(`No user with id ${userId}`);
+  }
+  res.status(StatusCodes.OK).json({ success: true });
 };
-
 
 const createUser = async (req, res) => {
-    const user = await User.create(req.body);
-    res.status(StatusCodes.CREATED).json({ user });
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ user });
 };
 const updateUser = async (req, res) => {
-    const {
-        body: { first_name, last_name, email },
-        params: { id:userId },
-    } = req;
+  const {
+    body: { first_name, last_name, email },
+    params: { id: userId },
+  } = req;
 
-    const user = await User.findByIdAndUpdate(
-        { _id: userId },
-        req.body,
-        { new: true, runValidators: true }
-    );
-    if (!user) {
-        throw new NotFoundError(`No user with id ${userId}`);
-    }
-    res.status(StatusCodes.OK).json({ user });
+  const user = await User.findByIdAndUpdate({ _id: userId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!user) {
+    throw new NotFoundError(`No user with id ${userId}`);
+  }
+  res.status(StatusCodes.OK).json({ user });
 };
 
 module.exports = {
-    getAllUsers,
-    getUser,
-    createUser,
-    updateUser,
-    deleteUser,
+  getAllUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
 };
